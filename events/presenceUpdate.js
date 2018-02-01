d02f0691a63eabe3f0745b93efd0518ed730db1a
guylaine <guylaine@genclik.ca>
2018-02-01 09:13:55 -0500

    Fix presence null

--- events/presenceUpdate.js
+++ events/presenceUpdate.js
@@ -3,9 +3,13 @@
 module.exports = (oldmember, member) => {
 
   // If the game is null, not being streamed or is the same person as before, don't proceed (don't send a streaming notif.)
-  if (member.user.presence.game == null && (member.user.presence.game.type != 1) && (oldmember.user.username != member.user.username)) {
-  	return;
+  if (member.user.presence.game == null && (oldmember.user.username != member.user.username)) {
+	return;
   }
+
+  if(member.user.presence.game.type != 1) {
+  		return;
+  	}
 
   const streamMessage = `OYÉ! OYÉ! @everyone ! ${member.user.username} started streaming ${member.user.presence.game.name}. Go check it out! ${member.user.presence.game.url}`;
 
