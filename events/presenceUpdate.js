702d016575a36736be79397c9feb8866de92497c
guylaine <guylaine@genclik.ca>
2018-02-02 15:32:48 -0500

    Try to fix the spamming

--- events/presenceUpdate.js
+++ events/presenceUpdate.js
@@ -1,15 +1,17 @@
 // This event executes when the presence of a member change!
 
-module.exports = (oldmember, member) => {
+module.exports = (client, member) => {
 
   // If the game is null, not being streamed or is the same person as before, don't proceed (don't send a streaming notif.)
-  if (member.user.presence.game == null && (oldmember.user.username != member.user.username)) {
-	return;
-  }
-
-  if(member.user.presence.game.type != 1) {
-  		return;
+  if(!member.presence.equals(member.user.presence)){
+  	if (member.user.presence.game == null) {
+		return;
   	}
+
+  	if(member.user.presence.game.type != 1) {
+		return;
+	}
+  }
 
   const streamMessage = `OYÉ! OYÉ! @everyone ! ${member.user.username} started streaming ${member.user.presence.game.name}. Go check it out! ${member.user.presence.game.url}`;
 
